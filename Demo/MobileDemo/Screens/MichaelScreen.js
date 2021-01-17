220dbc947dea3a5ec669fc5c2479bbb6adc63023
Michael Azzopardi <michael.azzopardi.18@um.edu.mt>
2021-01-17 09:43:06 +0100

    Michaels Commit
    For Text and Sound Demo

--- Demo/MobileDemo/Screens/MichaelScreen.js
+++ Demo/MobileDemo/Screens/MichaelScreen.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 
-export default function MichaelScreen() {
+export default function MichaelScreen() { 
+
     return (
-     <View>
-         <Text>Hello Michael</Text>
-     </View>
+        <View>
+            <Text>This is the Title of the Demo</Text>
+            <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer 
+                took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+                but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s 
+                with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software 
+                ike Aldus PageMaker including versions of Lorem Ipsum.</Text>
+            
+        </View>
     );
-} 
+}   
+
+
+
+{/* <Card >
+                <Card.Content>
+                    <Title>Card Title</Title>
+                    <Paragraph>This is some content of the above title</Paragraph>
+                </Card.Content>
+                <Card.Actions>
+                        <Button onPress={playSoundTap}>Share</Button>
+                        <Button onPress={playSoundComplete}>Explore</Button>
+                </Card.Actions>
+            </Card> */}
